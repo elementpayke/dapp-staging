3da66c0e971f46b0c638d9aa84ced25a84bbb509
daveclinton <clintondavid46@gmail.com>
2025-05-22 14:38:35 +0300

    feat: separate exhange loader

--- src/components/dashboard/QuickActions.tsx
+++ src/components/dashboard/QuickActions.tsx
@@ -1,52 +1,24 @@
 "use client";
-import React, { FC, useState, useEffect } from "react";
-import {
-  ArrowDown,
-  ArrowDownLeft,
-  ArrowRightLeft,
-  ArrowUpRight,
-  Bell,
-  MoreHorizontal,
-  Wallet,
-} from "lucide-react"; // Import icons
-import { useWallet } from "@/context/WalletContext";
+import React, { FC } from "react";
+import { Bell, MoreHorizontal } from "lucide-react"; // Import icons
+
 import SendCryptoModal from "./sendCrypto/SendCryptoModal";
 import DepositCryptoModal from "./depositCrypto/DepositCryptoModal";
+import { useWallet } from "@/hooks/useWallet";
+import { useExchangeRate } from "@/lib/useExchangeRate";
 
 const QuickActions: FC = () => {
-  const [exchangeRate, setExchangeRate] = useState<number | null>(null);
-  const { usdcBalance } = useWallet(); // Fetch USDC balance from wallet context
-  const MARKUP_PERCENTAGE = 1.5; // 1.5% markup
+  const { usdcBalance } = useWallet();
+  const { exchangeRate, isLoading } = useExchangeRate();
+  const formattedKesBalance = () => {
+    if (isLoading || !exchangeRate) return "Loading...";
 
-  // Fetch exchange rate from Coinbase API
-  useEffect(() => {
-    const fetchExchangeRate = async () => {
-      try {
-        const response = await fetch(
-          "https://api.coinbase.com/v2/exchange-rates?currency=USDC"
-        );
-        const data = await response.json();
-        if (data?.data?.rates?.KES) {
-          const baseRate = parseFloat(data.data.rates.KES);
-          const markupRate = baseRate * (1 - MARKUP_PERCENTAGE / 100);
-          setExchangeRate(markupRate);
-        } else {
-          console.error("KES rate not found");
-          setExchangeRate(null);
-        }
-      } catch (error) {
-        console.error("Error fetching exchange rate:", error);
-        setExchangeRate(null);
-      }
-    };
-
-    fetchExchangeRate();
-  }, []);
-
-  // Calculate fiat wallet balance in KES
-  const fiatBalanceKES = exchangeRate
-    ? (usdcBalance * exchangeRate).toFixed(2)
-    : "Loading...";
+    const kesAmount = usdcBalance * exchangeRate;
+    return new Intl.NumberFormat("en-KE", {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2,
+    }).format(kesAmount);
+  };
 
   return (
     <div className="p-6 bg-white shadow-lg rounded-2xl border border-gray-100">
@@ -54,7 +26,8 @@ const QuickActions: FC = () => {
         <div>
           <p className="text-sm text-gray-600 mb-1">Fiat Balance</p>
           <p className="text-3xl font-bold text-gray-900">
-            KES <span className="text-emerald-600">{fiatBalanceKES}</span>
+            KES{" "}
+            <span className="text-emerald-600">{formattedKesBalance()}</span>
           </p>
         </div>
         <div className="flex gap-2">
